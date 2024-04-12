31752faf41a381c9ffb6db30c1028b2a57aa1659
Alvarito2222 <aleonp00@gmail.com>
2024-04-12 00:21:30 -0400

    comitt pictures

--- server.js
+++ server.js
@@ -7,17 +7,12 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Specific route for the PDF file
-app.get('/AlvaroResume.pdf', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'AlvaroResume.pdf'));
-});
-
-// Handle SPA routing by returning the index.html for all other routes
+// Handle SPA routing by returning the index.html for all routes
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'src', 'index.html')); // Adjust this path based on where your index.html is located
 });
 
 const PORT = process.env.PORT || 3000;
